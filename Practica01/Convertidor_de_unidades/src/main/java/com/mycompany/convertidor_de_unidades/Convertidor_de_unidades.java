1713469a85e9ac922587645f2dc8ef36ac3420ae
LuIs Diaz <ladch02jun.2@gmail.com>
2026-04-25 20:25:23 -0600

    Menu
    Realizamos el cascaron del programa donde sembraremos la logica de  las conversiones de unidades basicas

--- Practica01/Convertidor_de_unidades/src/main/java/com/mycompany/convertidor_de_unidades/Convertidor_de_unidades.java
+++ Practica01/Convertidor_de_unidades/src/main/java/com/mycompany/convertidor_de_unidades/Convertidor_de_unidades.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.convertidor_de_unidades;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,31 @@ package com.mycompany.convertidor_de_unidades;
 public class Convertidor_de_unidades {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Convertidor de unidades basicas\n");
+        System.out.println("-------MENU------\n");
+        System.out.println("1)Metro\n2)Kilo\n3)Litro\n4)Horas\n5)Salir\nOpcion: ");
+        int opcion = leer.nextInt();
+        while(opcion != 5)
+        {
+            switch(opcion)
+            {
+                case 1:
+                    System.out.println("Ingrese el numero de metros a convertir a cm,km");
+                    break;
+                case 2:
+                    System.out.println("Ingrese el numero de kilos a convertir a gr,toneladas");
+                    break;
+                case 3:
+                    System.out.println("Ingrese el numero de litros a convertir a ml");
+                    break;
+                case 4:
+                    System.out.println("Ingrese el numero de horas a convertir segundos, minutos");
+                    break;
+                default: System.out.println("Opcion no  valida");
+            }
+        }
+        
     }
 }
