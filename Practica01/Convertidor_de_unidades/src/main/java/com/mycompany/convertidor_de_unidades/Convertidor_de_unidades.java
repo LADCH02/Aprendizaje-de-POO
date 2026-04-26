b202ba358d60d21fca861496ce43a2078204119a
LuIs Diaz <ladch02jun.2@gmail.com>
2026-04-25 19:55:51 -0600

    Convertidor
    Creamos el proyecto donde vamos estar trabajando para realizar el convertidor de unidades.

---
+++ Practica01/Convertidor_de_unidades/src/main/java/com/mycompany/convertidor_de_unidades/Convertidor_de_unidades.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.convertidor_de_unidades;
+
+/**
+ *
+ * @author luis
+ */
+public class Convertidor_de_unidades {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
