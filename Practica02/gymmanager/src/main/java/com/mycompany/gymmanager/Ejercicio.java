e4c1ade3404e4b20e2b764238fe577c2eb61d7f6
LuIs Diaz <ladch02jun.2@gmail.com>
2026-04-27 09:43:35 -0600

    Constructor
    Creamos un constructor donde le pasamos los datos basicos del ejercicio, este constructor hace referencia a un llenado por parte de un persona que si conoce todo los ejercicios.

---
+++ Practica02/gymmanager/src/main/java/com/mycompany/gymmanager/Ejercicio.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gymmanager;
+
+/**
+ *
+ * @author luis
+ */
+public class Ejercicio {
+    String nombre;
+    String grupo_muscular;
+    int series;
+    int repeticiones;
+    double pesoKg;
+    int rpe;
+    
+    
+    public Ejercicio(String nombre, String grupo_muscular, int series, int repeticiones, double pesokg, int rpe){
+        this.nombre = nombre;
+        this.grupo_muscular = grupo_muscular;
+        this.series = series;
+        this.repeticiones = repeteciones;
+        this.pesoKg = pesokg;
+        this.rpe = rpe;
+    }
+    
+    
+    
+    
+    
+    
+}
