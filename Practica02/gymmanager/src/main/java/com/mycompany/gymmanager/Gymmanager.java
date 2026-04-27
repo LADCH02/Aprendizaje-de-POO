ed64bf174173ff4229050ded9677cd84c43689d4
LuIs Diaz <ladch02jun.2@gmail.com>
2026-04-27 09:30:53 -0600

    Practica 2
    Creamos la carpeta donde estara la practica dos.

---
+++ Practica02/gymmanager/src/main/java/com/mycompany/gymmanager/Gymmanager.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gymmanager;
+
+/**
+ *
+ * @author luis
+ */
+public class Gymmanager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+    }
+}
