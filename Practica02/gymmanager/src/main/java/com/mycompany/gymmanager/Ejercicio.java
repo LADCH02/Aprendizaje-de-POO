abc7961e38579d059d3de4d8080784b6a26d9208
Luls Diaz <ladch02jun.2@gmail.com>
2026-04-28 12:17:56 -0600

    Realizamos los metodos de istancia en este caso abordamos, metodos que solo tuvieran cosas relevantes para el progreso de un ejercicio como tal

--- Practica02/gymmanager/src/main/java/com/mycompany/gymmanager/Ejercicio.java
+++ Practica02/gymmanager/src/main/java/com/mycompany/gymmanager/Ejercicio.java
@@ -36,12 +36,38 @@ public class Ejercicio {
     }
     
     
-       public Ejercicio(){
+    public Ejercicio(){
         this.nombre = "Ejercicio generico";
         this.grupo_muscular = "Cuerpo completo";
     }
     
     // 1) progreso de peso, 2) fallo muscular o cerca del fallo muscular, 3) Cambiar rep y ser, 4) obtener resumen , 5) obtener el nombre;
-       
     
+    
+    public void progreso_peso(double pesoextra){
+        pesoKg += pesoextra;
+        System.out.println("En el ejercicio: " + nombre + "se obtuvo un nuevo peso de " + pesoKg);
+    }
+    
+    public void fallo_muscular(int rpe){
+        if(rpe > 8)
+            System.out.println("En el ejercicio: " + nombre + " se encuentra apunto del fallo muscular con un RPE de " + rpe + "tenga cuidado puede tener una sobre carga nerviosa");
+        else
+            System.out.println("En el ejercicio: " + nombre + " se encuentra en un RPE optimo de " + rpe + " sin problema de tener una sobre carga");      
+    }
+    
+    public void cambiar_parametros(int rpe, int series){
+        this.rpe = rpe;
+        this.series = series;
+    }
+    
+    public String resumen(){
+        return ("El grupo muscular es " + grupo_muscular + "\nEjercicio: " + nombre + "\nSeries: " + series + "\nRepeticiones: " + repeticiones + "\nEl peso maximo fue: " + pesoKg + "\nEl maximo RPE: " + rpe + "\nMuchas felicidades por su esfuerzo son unos maravillosos pesos los obtenidos vamos por mas");
+    }
+    
+    
+    public String getnombre(){
+        return nombre;
+    }
 }
+
