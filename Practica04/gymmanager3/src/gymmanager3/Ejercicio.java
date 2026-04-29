6df879b4fc5085442d696747506c11e19c523a53
Luls Diaz <ladch02jun.2@gmail.com>
2026-04-29 15:34:44 -0600

    Hacemos que la clase Ejercicio se convierta en clase abstracta y cambios los datos protected para que sus clases hijas puedan acceder y hacer uso de la herencia en este caso nuestra clase Ejercicio seria nuestra primera capa o en este caso de 3 capas nuestra clase abuelo, taambien eliminamos, ciertos datos de entrada que se pasaron la clase EjercicioFuerza que es la clase de la capa 2 el padre, en ella vamos implementar los datos que borramos antes debido a que se observo que esos datos no deberian de estar en la clase abuelo por ser datos especificos de un tipo de ejercicio.

--- Practica04/gymmanager3/src/gymmanager3/Ejercicio.java
+++ Practica04/gymmanager3/src/gymmanager3/Ejercicio.java
@@ -8,22 +8,18 @@ package gymmanager3;
  *
  * @author FCFM
  */
-public class Ejercicio {
-    private String nombre;
-    private String grupo_muscular;
-    private int series;
-    private int repeticiones;
-    private double pesoKg;
-    private int rpe;
+public abstract class Ejercicio {
+    protected String nombre;
+    protected String grupo_muscular;
+    protected int series;
+    protected int repeticiones;
     
     
-    public Ejercicio(String nombre, String grupo_muscular, int series, int repeticiones, double pesokg, int rpe){
+    public Ejercicio(String nombre, String grupo_muscular, int series, int repeticiones){
         setNombre(nombre);
         this.grupo_muscular = grupo_muscular;
         this.series = series;
         this.repeticiones = repeticiones;
-        setPesoKg(pesoKg);
-        this.rpe = rpe;
     }
     
     public Ejercicio(String nombre, String grupo_muscular){
@@ -31,8 +27,6 @@ public class Ejercicio {
         this.grupo_muscular = grupo_muscular;
         this.series = 3;
         this.repeticiones = 10;
-        setPesoKg(1.0);
-        this.rpe = 0;
     }
     
     
@@ -42,6 +36,8 @@ public class Ejercicio {
     }
     
     
+    public abstract void ejecutar();
+    
     public void setNombre(String nombre){
         if(nombre != null && nombre.trim().length() >= 3)
             this.nombre = nombre;
@@ -49,22 +45,11 @@ public class Ejercicio {
             System.out.println("El nombre es muy corto no es valido " + nombre);
     }
     
-    
-    public void setPesoKg(double pesoKg){
-        if(pesoKg > 0 && pesoKg < 300)
-            this.pesoKg = pesoKg;
-        else
-            System.out.println("El peso seleccionado no es valido: " + pesoKg);
-    }
-    
-    
     public String getNombre(){
         return this.nombre;
     }
     
-    public double getPesoKg(){
-        return this.pesoKg;
-    }
+
     
     
     
@@ -72,10 +57,6 @@ public class Ejercicio {
     // 1) progreso de peso, 2) fallo muscular o cerca del fallo muscular, 3) Cambiar rep y ser, 4) obtener resumen , 5) obtener el nombre;
     
     
-    public void progreso_peso(double pesoextra){
-        pesoKg += pesoextra;
-        System.out.println("En el ejercicio: " + nombre + "se obtuvo un nuevo peso de " + pesoKg);
-    }
     
     public void fallo_muscular(int rpe){
         if(rpe > 8)
@@ -83,18 +64,13 @@ public class Ejercicio {
         else
             System.out.println("En el ejercicio: " + nombre + " se encuentra en un RPE optimo de " + rpe + " sin problema de tener una sobre carga");      
     }
-    
-    public void cambiar_parametros(int rpe, int series){
-        this.rpe = rpe;
-        this.series = series;
-    }
-    
+   
     public String resumen(){
-        return ("El grupo muscular es " + grupo_muscular + "\nEjercicio: " + nombre + "\nSeries: " + series + "\nRepeticiones: " + repeticiones + "\nEl peso maximo fue: " + pesoKg + "\nEl maximo RPE: " + rpe + "\nMuchas felicidades por su esfuerzo son unos maravillosos pesos los obtenidos vamos por mas");
+        return ("El grupo muscular es " + grupo_muscular + "\nEjercicio: " + nombre + "\nSeries: " + series + "\nRepeticiones: " + repeticiones + "\nMuchas felicidades por su esfuerzo son unos maravillosos pesos los obtenidos vamos por mas");
     }
     
     @Override
     public String toString(){
-        return String.format("Ejercicio: %s | Musculo: %s | Carga: %.2fkg (%dx%d)",nombre,grupo_muscular,pesoKg,series,repeticiones);
+        return String.format("Ejercicio: %s | Musculo: %s | Carga: (%dx%d)",nombre,grupo_muscular,series,repeticiones);
     }
 }
--- Practica04/gymmanager3/src/gymmanager3/Gymmanager3.java
+++ Practica04/gymmanager3/src/gymmanager3/Gymmanager3.java
@@ -16,22 +16,8 @@ public class Gymmanager3 {
     public static void main(String[] args) {
         Rutina Entrenamiento = new Rutina("Dia de pierna");
         
-        Ejercicio e1 = new Ejercicio("Sentadilla","Pierna completa",4,10,100.0,7);
-        Ejercicio e2 = new Ejercicio("Peso muerto", "Pierna trasera",4,10,70.0,8);
-        Ejercicio e3 = new Ejercicio("Sentadilla isometrica", "Tendones",4,1,20.0,8);
-        Ejercicio e4 = new Ejercicio("Extension de cuadricep", "Cuadricep",4,10,80.0,7);
-        Ejercicio e5 = new Ejercicio("Curl femoral", "Femorales",4,10,70.0,7);
         
         
-        e1.fallo_muscular(8);
-        e2.progreso_peso(20);
-        
-        
-        Entrenamiento.agregarEjercicio(e1);
-        Entrenamiento.agregarEjercicio(e2);
-        Entrenamiento.agregarEjercicio(e3);
-        Entrenamiento.agregarEjercicio(e4);
-        Entrenamiento.agregarEjercicio(e5);
         
         System.out.println("Buscando un ejercicio en particular");
         Entrenamiento.buscarEjercicio("Sentadilla");
@@ -42,14 +28,6 @@ public class Gymmanager3 {
         
         // Practica 3
         
-        System.out.println("--- Prueba de nombre---");
-        e1.setNombre("X");
         
-        System.out.println("----Prueba de peso----");
-        e1.setPesoKg(-1.0);
-        
-        System.out.println("----Prueba de composicion----");
-        EjercicioPersonalizado Rutina1 = new EjercicioPersonalizado("El objetivo de este ejercicio fortalecer el rango de movimiento, tambien la fuerza de empuje de las piernas.", 20,e1);
-        Rutina1.detalles();
     }  
 }
