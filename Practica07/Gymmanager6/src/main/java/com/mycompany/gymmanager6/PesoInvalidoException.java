416993c2bd08e96b582553ec760cf4ff33f0a6fe
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-07 22:00:57 -0600

    Le agregamos funcionalidad a una excpcion.

--- Practica07/Gymmanager6/src/main/java/com/mycompany/gymmanager6/PesoInvalidoException.java
+++ Practica07/Gymmanager6/src/main/java/com/mycompany/gymmanager6/PesoInvalidoException.java
@@ -4,20 +4,29 @@
  */
 package com.mycompany.gymmanager6;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Lic Carmen
  */
 public class PesoInvalidoException extends GymException{
     private double valorErroneo;
+    private final LocalDateTime horaDelError; 
+    private final String codigoInterno; //
     
     public PesoInvalidoException(String mensaje, double valor){
         super(mensaje);
         this.valorErroneo = valor;
+        this.horaDelError = LocalDateTime.now();
+        this.codigoInterno = "GYM-ERR-101";
     }
     
     public double getvalorErroneo(){
         return valorErroneo;
     }
+    
+    public LocalDateTime getHoraDelError() { return horaDelError; }
+    public String getCodigoInterno() { return codigoInterno; }
     
 }
