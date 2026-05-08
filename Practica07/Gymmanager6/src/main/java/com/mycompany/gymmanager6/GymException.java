f757a0168df1c43afcdebcb13a9990f3790f1b6d
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-07 19:06:45 -0600

    Creamos una clase base para que maneje todos los errores.

---
+++ Practica07/Gymmanager6/src/main/java/com/mycompany/gymmanager6/GymException.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gymmanager6;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Lic Carmen
+ */
+public class GymException extends Exception{
+    private LocalDateTime times;
+    
+    public GymException(String mensaje){
+        super(mensaje);
+        this.times = LocalDateTime.now();
+    }
+    
+    public LocalDateTime gettimes(){
+        return times;
+    }
+}
