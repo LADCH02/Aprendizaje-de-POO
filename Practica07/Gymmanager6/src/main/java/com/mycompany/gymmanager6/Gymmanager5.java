e92852f5ba5f3b00271199483670dc4960526700
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-07 20:18:24 -0600

    Realizamos un pequeno try-catch para poder ver como reacciona nuestras excepciones ante una lista de pesos dados.

--- Practica07/Gymmanager6/src/main/java/com/mycompany/gymmanager6/Gymmanager5.java
+++ Practica07/Gymmanager6/src/main/java/com/mycompany/gymmanager6/Gymmanager5.java
@@ -11,7 +11,30 @@ package com.mycompany.gymmanager6;
 public class Gymmanager5 {
 
     public static void main(String[] args) {
-        
-        
+        Rutina miRutina = new Rutina("Entrenamiento de Potencia");
+
+        double[] pesosDePrueba = {100.0, -50.0, 600.0};
+
+        for (double p : pesosDePrueba) {
+            try {
+                System.out.println("\nIntentando registrar peso: " + p);
+                miRutina.registrarEntrenamiento(p);
+                System.out.println("¡Registro exitoso!");
+                
+            } catch (PesoInvalidoException e) {
+                // Accedemos a la información de contexto adicional
+                System.err.println("[ERROR] " + e.getMessage());
+                System.err.println("Valor que causó el error: " + e.getvalorErroneo());
+                System.err.println("Hora del error: " + e.gettimes());
+                
+            } catch (CapacidadExcedidaException e) {
+                System.err.println("[ALERTA DE SEGURIDAD] " + e.getMessage());
+                
+            } catch (GymException e) {
+                System.err.println("[ERROR GENERAL] " + e.getMessage());
+            } finally {
+                System.out.println("Finalizando intento de registro.");
+            }
+        }
     }
 }
