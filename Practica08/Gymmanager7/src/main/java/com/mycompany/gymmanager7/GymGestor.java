96f1117b0b2a1688794dfc0f783c9aa3c37a79da
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-08 22:24:22 -0600

    Se realiza una implementacion de las funciones restantes del crud.

--- Practica08/Gymmanager7/src/main/java/com/mycompany/gymmanager7/GymGestor.java
+++ Practica08/Gymmanager7/src/main/java/com/mycompany/gymmanager7/GymGestor.java
@@ -4,10 +4,83 @@
  */
 package com.mycompany.gymmanager7;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author Lic Carmen
  */
 public class GymGestor {
+    private List<Ejercicio> inventario = new ArrayList<>();
+    private Set<String> gruposMusculares = new HashSet<>();
+    private Map<String, Ejercicio> mapaBusqueda = new HashMap<>();
+    private Queue<Ejercicio> colaMantenimiento = new LinkedList<>();
+
+    
+    
+    public void agregarEjercicio(Ejercicio e) {
+        inventario.add(e);
+        gruposMusculares.add(e.getGrupoMuscular());
+        mapaBusqueda.put(e.getNombre().toLowerCase(), e);
+    }
+
+    
+    public List<Ejercicio> filtrarAvanzado(String grupo, double minCalorias) {
+        return inventario.stream()
+                .filter(e -> e.getGrupoMuscular().equalsIgnoreCase(grupo))
+                .filter(e -> e.calcularCalorias() >= minCalorias)
+                .collect(Collectors.toList());
+    }
+
+    public void mostrarTodo() {
+       
+        Iterator<Ejercicio> it = inventario.iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next().resumen());
+        }
+    }
+    
+    
+    public boolean eliminarEjercicio(String nombre) {
+        String clave = nombre.toLowerCase(); 
+        
+        if (mapaBusqueda.containsKey(clave)) {
+            
+            Ejercicio ejercicioABorrar = mapaBusqueda.get(clave);
+            
+            mapaBusqueda.remove(clave);
+            inventario.remove(ejercicioABorrar);
+            colaMantenimiento.remove(ejercicioABorrar);
+     
+            System.out.println("Ejercicio '" + nombre + "' eliminado correctamente.");
+            return true;
+        } else {
+            System.out.println("No se encontró el ejercicio '" + nombre + "' para eliminar.");
+            return false;
+        }
+    }
+    
+    
+    public boolean actualizarEjercicio(String nombreViejo, Ejercicio nuevoEjercicio) {
+        String clave = nombreViejo.toLowerCase();
+        
+        if (mapaBusqueda.containsKey(clave)) {
+          
+            Ejercicio viejo = mapaBusqueda.get(clave);
+            inventario.remove(viejo);
+            mapaBusqueda.remove(clave);
+            colaMantenimiento.remove(viejo);
+            
+           
+            agregarEjercicio(nuevoEjercicio);
+            
+            System.out.println("El ejercicio '" + nombreViejo + "' ha sido actualizado con éxito.");
+            return true;
+        } else {
+            System.out.println("Error al actualizar: No existe el ejercicio '" + nombreViejo + "'.");
+            return false;
+        }
+    }
     
 }
