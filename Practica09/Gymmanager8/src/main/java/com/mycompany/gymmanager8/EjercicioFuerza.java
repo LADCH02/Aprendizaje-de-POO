cba793de49824d135f422b2abd2e88d852cebfd7
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-09 18:48:01 -0600

    Se crea la carpeta para la practica 9 y se copia todo lo de practica pasada a la nueva.

---
+++ Practica09/Gymmanager8/src/main/java/com/mycompany/gymmanager8/EjercicioFuerza.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gymmanager8;  
+
+/**
+ *
+ * @author Lic Carmen
+ */
+public abstract class EjercicioFuerza extends Ejercicio{
+    protected double pesoKg;
+    protected int rpe;
+    
+    public EjercicioFuerza(String nombre, String grupo_muscular, int series, int repeticiones, double pesoKg, int rpe){
+        super(nombre, grupo_muscular, series, repeticiones);
+        setPesoKg(pesoKg);
+        this.rpe = rpe;
+    }
+    
+    
+    public void setPesoKg(double pesoKg){
+        this.pesoKg = pesoKg;
+    }
+    
+    
+    public void progreso_peso(double pesoextra){
+        pesoKg += pesoextra;
+        System.out.println("En el ejercicio: " + nombre + "se obtuvo un nuevo peso de " + pesoKg);
+    }
+    
+    public void cambiar_parametros(int rpe, int series){
+        this.rpe = rpe;
+        this.series = series;
+    }
+}
---
+++ Practica09/Gymmanager8/src/main/java/com/mycompany/gymmanager8/Gymmanager8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gymmanager8;
+
+/**
+ *
+ * @author Lic Carmen
+ */
+public class Gymmanager8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
