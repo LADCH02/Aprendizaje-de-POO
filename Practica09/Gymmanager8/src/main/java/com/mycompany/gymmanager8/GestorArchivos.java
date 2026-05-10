20b3e4eb9b9d006c0477c87f06ee3685ea1ed340
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-09 19:16:26 -0600

    Se realiza constructor para tener listo el directorio y en caso de que ya exista simplemente no crea uno nuevo para trabajar con el mismo.

--- Practica09/Gymmanager8/src/main/java/com/mycompany/gymmanager8/GestorArchivos.java
+++ Practica09/Gymmanager8/src/main/java/com/mycompany/gymmanager8/GestorArchivos.java
@@ -4,10 +4,21 @@
  */
 package com.mycompany.gymmanager8;
 
+import java.io.File;
+
 /**
  *
  * @author Lic Carmen
  */
 public class GestorArchivos {
+    private static final String DIRECTORIO_DATOS = "datos_gimnasio";
+    private static final String ARCHIVO_BINARIO = DIRECTORIO_DATOS + "/inventario.dat";
     
+    public GestorArchivos() {
+        
+        File directorio = new File(DIRECTORIO_DATOS);
+        if (!directorio.exists()) {
+            directorio.mkdirs();
+        }
+    }
 }
