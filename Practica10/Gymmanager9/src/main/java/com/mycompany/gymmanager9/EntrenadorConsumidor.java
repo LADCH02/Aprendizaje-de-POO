757eb297d7c1013c67c44576d565fd0f3aee83fe
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-13 21:23:25 -0600

    Realizamos excatemente lo mismo solo que para el consumidor y hacemos uso del metodo atenderclientes, tambien se puede observar que en este caso implementamos thread para poder entender un poco sus diferencias, no se permite herencia multimple.

--- Practica10/Gymmanager9/src/main/java/com/mycompany/gymmanager9/EntrenadorConsumidor.java
+++ Practica10/Gymmanager9/src/main/java/com/mycompany/gymmanager9/EntrenadorConsumidor.java
@@ -8,6 +8,24 @@ package com.mycompany.gymmanager9;
  *
  * @author Lic Carmen
  */
-public class EntrenadorConsumidor {
-    
+public class EntrenadorConsumidor extends Thread{
+    private ZonaEspera zonaEspera;
+    private String nombreEntrenador;
+
+    public EntrenadorConsumidor(ZonaEspera zonaEspera, String nombreEntrenador) {
+        this.zonaEspera = zonaEspera;
+        this.nombreEntrenador = nombreEntrenador;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i <= 8; i++) {
+                zonaEspera.atenderCliente(nombreEntrenador);
+                Thread.sleep((long) (Math.random() * 1000 + 1000));
+            }
+        } catch (InterruptedException e) {
+            System.err.println(nombreEntrenador + " terminó su turno.");
+        }
+    }
 }
