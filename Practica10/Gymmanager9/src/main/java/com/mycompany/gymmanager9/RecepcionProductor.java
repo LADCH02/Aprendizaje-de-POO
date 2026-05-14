a50b090017379ca8ec5aa53dc4ea11c3044bb7ec
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-13 21:02:09 -0600

    Se realiza un constructor y un metodo por defecto de runnable, run, hacemos uso de try-catch para manejo de excepciones por interrupcion, tambien realizamos en el try un for para 8 clientes, hacemos uso de agregarcliente y le mandamos los parametros correspondientes cliente y nombrerecepcion, realiza todo lo ya explicado en dos commits atras y luego realizamos una opearcion para realizar una simulaciuon de tiempo.

--- Practica10/Gymmanager9/src/main/java/com/mycompany/gymmanager9/RecepcionProductor.java
+++ Practica10/Gymmanager9/src/main/java/com/mycompany/gymmanager9/RecepcionProductor.java
@@ -8,6 +8,25 @@ package com.mycompany.gymmanager9;
  *
  * @author Lic Carmen
  */
-public class RecepcionProductor {
-    
+public class RecepcionProductor implements Runnable{
+    private ZonaEspera zonaEspera;
+    private String nombreRecepcion;
+
+    public RecepcionProductor(ZonaEspera zonaEspera, String nombreRecepcion) {
+        this.zonaEspera = zonaEspera;
+        this.nombreRecepcion = nombreRecepcion;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i <= 8; i++) {
+                String cliente = "Cliente-" + i + " de " + nombreRecepcion;
+                zonaEspera.agregarCliente(cliente, nombreRecepcion);
+                Thread.sleep((long) (Math.random() * 1000 + 500));
+            }
+        } catch (InterruptedException e) {
+            System.err.println(nombreRecepcion + " fue interrumpida.");
+        }
+    }
 }
