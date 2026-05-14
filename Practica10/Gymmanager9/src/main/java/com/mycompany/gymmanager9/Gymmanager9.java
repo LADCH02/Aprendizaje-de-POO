5d440fc49c8ac15b6d47c708aef1fe7e36c2c377
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-14 13:11:47 -0600

    Se realiza en el main el uso del diferentes objetos que fuimos creando, cramos un objeto de zonaespera para crear un puente donde se dejan a los clientes, realizamos un llenado, luego realizamos entrenador para pasarlos, luego hacemos uso .start para crear hilos para que puedan trabajar en salaespera,luego hacemos uso de executorservicec para crear dos hilos y trabajar con 2 maquinas a la ves, luego en ciclo le mandamos la lsta de string para personaslimpieza empiece a trabajar, luego hacemos uso shutdown paara que los hilos no se queden esperando.

--- Practica10/Gymmanager9/src/main/java/com/mycompany/gymmanager9/Gymmanager9.java
+++ Practica10/Gymmanager9/src/main/java/com/mycompany/gymmanager9/Gymmanager9.java
@@ -3,6 +3,8 @@
  */
 
 package com.mycompany.gymmanager9;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  *
@@ -11,6 +13,33 @@ package com.mycompany.gymmanager9;
 public class Gymmanager9 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("===== ABRIENDO GIMNASIO (HORA PICO) =====\n");
+
+        ZonaEspera salaEspera = new ZonaEspera();
+
+       
+        Thread recepcion1 = new Thread(new RecepcionProductor(salaEspera, "Recepción A"));
+        Thread recepcion2 = new Thread(new RecepcionProductor(salaEspera, "Recepción B"));
+        
+        EntrenadorConsumidor entrenador1 = new EntrenadorConsumidor(salaEspera, "Entrenador Carlos");
+        EntrenadorConsumidor entrenador2 = new EntrenadorConsumidor(salaEspera, "Entrenadora Ana");
+
+        
+        recepcion1.start();
+        recepcion2.start();
+        entrenador1.start();
+        entrenador2.start();
+
+        
+        ExecutorService personalLimpieza = Executors.newFixedThreadPool(2);
+        String[] maquinasALimpiar = {"Press Banca", "Sentadilla Libre", "Trotadora 1", "Trotadora 2"};
+        
+        System.out.println("\n Mandando a limpiar máquinas en segundo plano..\n");
+        for (String maquina : maquinasALimpiar) {
+            personalLimpieza.execute(new LimpiezaTask(maquina));
+        }
+        
+        
+        personalLimpieza.shutdown();
     }
 }
