4e9e06c48d94078e7b389f73d06ca1de62755a50
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-13 23:10:16 -0600

    Se crea un metodo para limpiezatask en usamos una estructra similar ya trabajada en las anteriores metodos, hacemos uso del try-catch para manejo de excepciones, tambien hacemos uso de thread.currentthread para saber que hilo esta ejcutando la accion, tambien se puede apreciar el uso de sleep para pausar 2 seg el hilo.

--- Practica10/Gymmanager9/src/main/java/com/mycompany/gymmanager9/LimpiezaTask.java
+++ Practica10/Gymmanager9/src/main/java/com/mycompany/gymmanager9/LimpiezaTask.java
@@ -8,6 +8,21 @@ package com.mycompany.gymmanager9;
  *
  * @author Lic Carmen
  */
-public class LimpiezaTask {
-    
+public class LimpiezaTask implements Runnable{
+    private String maquina;
+
+    public LimpiezaTask(String maquina) {
+        this.maquina = maquina;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("[LIMPIEZA] Desinfectando " + maquina + " - Realizado por " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("[LIMPIEZA FIN] " + maquina + " lista para usarse.");
+    }
 }
