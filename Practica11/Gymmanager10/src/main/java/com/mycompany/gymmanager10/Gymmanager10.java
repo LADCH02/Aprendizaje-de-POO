ba17cda0deaa01d4c9c8bcfa1e73e327dad33e50
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-15 17:31:49 -0600

    Se realiza una serie de cambios en el pom se cambio el numero del manager como tambien usamos un lanzador, en ayuda de ia para poder solucinar este poblema.

--- Practica11/Gymmanager10/src/main/java/com/mycompany/gymmanager10/Gymmanager10.java
+++ Practica11/Gymmanager10/src/main/java/com/mycompany/gymmanager10/Gymmanager10.java
@@ -11,6 +11,6 @@ package com.mycompany.gymmanager10;
 public class Gymmanager10 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        GymAppGUI.main(args);
     }
 }
