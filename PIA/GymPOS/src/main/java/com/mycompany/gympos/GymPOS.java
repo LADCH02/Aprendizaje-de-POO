8cbf135cdc2ec2a5794e2abe7f3c5bbde961fcac
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-16 13:38:32 -0600

    Hacemos uso de nuestro paquete principal donde tenemos nuestra clase pricipiial en ella hacemos uso del metodo main pues este main nos servira como nuestro lanzador.

--- PIA/GymPOS/src/main/java/com/mycompany/gympos/GymPOS.java
+++ PIA/GymPOS/src/main/java/com/mycompany/gympos/GymPOS.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.gympos;
 
+import com.mycompany.gympos.main.GymPOSApp;
+
 /**
  *
  * @author Lic Carmen
@@ -11,6 +13,6 @@ package com.mycompany.gympos;
 public class GymPOS {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       GymPOSApp.main(args); 
     }
 }
