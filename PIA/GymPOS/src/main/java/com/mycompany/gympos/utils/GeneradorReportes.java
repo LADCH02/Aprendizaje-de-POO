3671c88b2cdafb5b90da7a7b2f4dea7475f1bed8
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-16 11:34:16 -0600

    Se me olvido guardar los cambios el commit anteriro representa el codigo que se presenta en este commit.

--- PIA/GymPOS/src/main/java/com/mycompany/gympos/utils/GeneradorReportes.java
+++ PIA/GymPOS/src/main/java/com/mycompany/gympos/utils/GeneradorReportes.java
@@ -4,10 +4,41 @@
  */
 package com.mycompany.gympos.utils;
 
+import com.mycompany.gympos.models.Cliente;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 /**
  *
  * @author Lic Carmen
  */
-public class GeneradorReportes {
-    
+public class GeneradorReportes extends Thread {
+    private List<Cliente> clientes;
+
+    public GeneradorReportes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Generando reporte en segundo plano...");
+        try {
+            
+            Thread.sleep(3000); 
+            
+            
+            try (FileWriter writer = new FileWriter("Reporte_Gimnasio.txt")) {
+                writer.write("=== REPORTE DE CLIENTES GYMPOS ===\n");
+                for (Cliente c : clientes) {
+                    String tipoMem = (c.getMembresia() != null) ? "Activa" : "Sin Membresía";
+                    writer.write("ID: " + c.getIdCliente() + " | Nombre: " + c.getNombre() + " | Puntos: " + c.getPuntosRecompensa() + " | Estado: " + tipoMem + "\n");
+                }
+                System.out.println("¡Reporte TXT generado exitosamente! (Revisa tu carpeta del proyecto)");
+            }
+            
+        } catch (InterruptedException | IOException e) {
+            System.err.println("Error en la generación del reporte: " + e.getMessage());
+        }
+    }
 }
