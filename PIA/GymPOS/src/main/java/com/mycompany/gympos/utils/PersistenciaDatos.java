381a409d759e41a49980aef0c1b5cf92456fb5df
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-15 19:19:07 -0600

    primero debemos ver que hacemos uso de import para poder traer la clase clientes, luego realizamos nuestro primer metodo guardarclientes, en este metodo le pasamos una lista de clientes y su ruta de archivo, hacemos uso del try para la creacion del objeto y cierre del mismo automatico, luego hacemos uso de objectoutputstream para serializar los datos para poder guardarlos como bytes, luego lo escribimos en oos con writeobject y madamos un mensaje de que se realio exitosamente. 2) Metodo 2, llamado cargarclientes nos servira para realizar lectura de clientes, el metodo se puede ver que va retornar una lista de datoos de tipo clientes, tambien recibe string de la ruta donde vamos a leer, luego creamos un objeto de tipo file con la ruta que que se mando, luego hacemos uso del metodo exists de file para saber si el archivo existe o no, si existe es true pero con ! sale y si no existe false pro con ! entra y mandamos un mensaje de no encontrado, y retornamos una lista vacia, en caso de que se encontro hacemos uso de la estructura try para poder creaar el objeto de lectura y que se cierre, retornamos dicho objeto ois como lista de clientes.

--- PIA/GymPOS/src/main/java/com/mycompany/gympos/utils/PersistenciaDatos.java
+++ PIA/GymPOS/src/main/java/com/mycompany/gympos/utils/PersistenciaDatos.java
@@ -3,11 +3,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.gympos.utils;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import com.mycompany.gympos.models.Cliente;
 
 /**
  *
  * @author Lic Carmen
  */
 public class PersistenciaDatos {
+  public static void guardarClientes(List<Cliente> clientes, String rutaArchivo) throws IOException {
+        
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(rutaArchivo))) {
+            oos.writeObject(clientes);
+            System.out.println("Datos de clientes guardados exitosamente.");
+        }
+    }
+
     
+    @SuppressWarnings("unchecked")
+    public static List<Cliente> cargarClientes(String rutaArchivo) throws IOException, ClassNotFoundException {
+        File archivo = new File(rutaArchivo);
+        
+        
+        if (!archivo.exists()) {
+            System.out.println("Archivo no encontrado. Se creará una base de datos nueva.");
+            return new ArrayList<>(); 
+        }
+        
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo))) {
+            System.out.println("Datos cargados correctamente.");
+            return (List<Cliente>) ois.readObject();
+        } 
+    }
 }
