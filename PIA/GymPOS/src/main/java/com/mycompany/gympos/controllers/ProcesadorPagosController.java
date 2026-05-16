4d9b549e47c58e256221f64fdaecb81aadb1fd21
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-16 00:49:00 -0600

    Creamos la clase procesadorpagoscontroller, en ella realizamos un constructor donde manejamos tres datos y hacemos uso de un if para realizar una pequena igualdad montopago es menor costomebresia si es verdad esto claramente deberia de ser invalido y madnamos un mensaje de fondos insuficientes, en caso de que el monto sea mayor entocnes mensaje en pantalla de apropbado tambien hacemos uso del metodo sumarpuntos para simular que le damos puntos.

--- PIA/GymPOS/src/main/java/com/mycompany/gympos/controllers/ProcesadorPagosController.java
+++ PIA/GymPOS/src/main/java/com/mycompany/gympos/controllers/ProcesadorPagosController.java
@@ -4,10 +4,20 @@
  */
 package com.mycompany.gympos.controllers;
 
+
+import com.mycompany.gympos.exceptions.PagoInvalidoException;
+import com.mycompany.gympos.models.Cliente;
 /**
  *
  * @author Lic Carmen
  */
 public class ProcesadorPagosController {
-    
+    public void procesarPagoMembresia(Cliente cliente, double montoPagado, double costoMembresia) throws PagoInvalidoException {
+        if (montoPagado < costoMembresia) {
+            throw new PagoInvalidoException("Fondos insuficientes. Faltan $" + (costoMembresia - montoPagado));
+        }
+        
+        System.out.println("Pago aprobado por $" + montoPagado + " para " + cliente.getNombre());
+        cliente.sumarPuntos(10); 
+    }
 }
