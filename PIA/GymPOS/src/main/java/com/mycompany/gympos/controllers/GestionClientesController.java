da936e08b162a02759bcc43d1fbbbfcf618850d0
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-16 00:39:25 -0600

    Se realiza un import de las clases clientes y persistenciasdatos, creamos dos variables una de tipo lista de clientes y otra string que contiene la dirrecion del archivo .dat, hacemos uso del metodo cargarclientes mandandole la ruta para guardar los datos en la lista, en caso de que pase una excepcion tenemos el catch, ahora creamos un metodo snecillo agregarcliente en el que mandamos un tipo de dato cliente, ese es guardado en la lista con el metodo .add y luego usamos guardar cambios, luego hacemos un metodo de busqueda en el que mandmos un string con que realizaremos la busqueda, realizamos un ciclo for donde recorremos la lista de los clientes y luego mediante un if mandamos la condicion dde c = idcliente con el metodo equals si es verdad realizamos getidcliente, posteriormente return de c, tercer metodo realizmos un geter de la lista de los clientes, cuarto metodo realizamos un metodo llamamdo guardarcambios en el realizmos una estructura try-catch donde podemos manejar las excepciones, dentro del try invocamos a guardarclientes y le mandamos sus respectivos datps de entrada.

--- PIA/GymPOS/src/main/java/com/mycompany/gympos/controllers/GestionClientesController.java
+++ PIA/GymPOS/src/main/java/com/mycompany/gympos/controllers/GestionClientesController.java
@@ -3,11 +3,52 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.gympos.controllers;
-
+import com.mycompany.gympos.models.Cliente;
+import com.mycompany.gympos.utils.PersistenciaDatos;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Lic Carmen
  */
 public class GestionClientesController {
-    
+    private List<Cliente> listaClientes;
+    private final String RUTA_ARCHIVO = "clientes.dat";
+
+    public GestionClientesController() {
+        try {
+            
+            listaClientes = PersistenciaDatos.cargarClientes(RUTA_ARCHIVO);
+        } catch (Exception e) {
+            listaClientes = new ArrayList<>();
+        }
+    }
+
+    public void agregarCliente(Cliente cliente) {
+        listaClientes.add(cliente);
+        guardarCambios();
+    }
+
+    public Cliente buscarCliente(String idCliente) {
+        for (Cliente c : listaClientes) {
+            if (c.getIdCliente().equals(idCliente)) {
+                return c;
+            }
+        }
+        return null; 
+    }
+
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+   
+    private void guardarCambios() {
+        try {
+            PersistenciaDatos.guardarClientes(listaClientes, RUTA_ARCHIVO);
+        } catch (IOException e) {
+            System.err.println("Error al guardar: " + e.getMessage());
+        }
+    }
 }
