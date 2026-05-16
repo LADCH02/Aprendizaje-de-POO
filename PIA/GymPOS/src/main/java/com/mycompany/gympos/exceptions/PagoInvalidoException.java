9c8f8b26d55acaeb1b80543a6690aa6751167b4b
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-15 18:49:41 -0600

    Realizamos lo mismo pero esta ves para manejar excecpiones por pago invalido.

---
+++ PIA/GymPOS/src/main/java/com/mycompany/gympos/exceptions/PagoInvalidoException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gympos.exceptions;
+
+/**
+ *
+ * @author Lic Carmen
+ */
+public class PagoInvalidoException extends Exception{
+    public PagoInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
