f122245a031edb2d89100242e84eeca425304919
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-15 18:20:23 -0600

    Se realiza abtraccion a membresia tambien se implementa serializable, se pude observqar qur creamos cuatros variables de tipo protected, dos local date, boolean y costo, tambien se realiza su constructor y sus respectivos getter, tambien se puede observar un metodo abstract que nos ayudar a que cada membresia debera de definir, y el meotodo renovar.

--- PIA/GymPOS/src/main/java/com/mycompany/gympos/models/Membresia.java
+++ PIA/GymPOS/src/main/java/com/mycompany/gympos/models/Membresia.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.gympos.models;
-
+import java.io.Serializable;
+import java.time.LocalDate;
 /**
  *
  * @author Lic Carmen
  */
-public class Membresia {
+public abstract class Membresia implements Serializable{
+    protected double costo;
+    protected LocalDate fechaInicio;
+    protected LocalDate fechaVencimiento;
+    protected boolean activa;
+
+    public Membresia(double costo, int diasDuracion) {
+        this.costo = costo;
+        this.fechaInicio = LocalDate.now();
+        this.fechaVencimiento = LocalDate.now().plusDays(diasDuracion);
+        this.activa = true;
+    }
+
     
+    public abstract String obtenerBeneficios();
+
+   
+    public double getCosto() { return costo; }
+    public LocalDate getFechaVencimiento() { return fechaVencimiento; }
+    public boolean isActiva() { return activa; }
+    
+    
+    public void renovar(int diasExtra) {
+        this.fechaVencimiento = this.fechaVencimiento.plusDays(diasExtra);
+        this.activa = true;
+    }
 }
