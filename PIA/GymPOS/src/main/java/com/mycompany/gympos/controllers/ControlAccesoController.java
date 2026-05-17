d69393a2c57df771d63dda4704b924d5755aaee2
LuIs Diaz <ladch02jun.2@gmail.com>
2026-05-16 22:37:28 -0600

    Se realiza una implementacion en registrarentrada, se hace usop de localdatetime.now para tener la hora actual con su respectivo formato, y se guarda el mensaje con un metodo nuevo creado, regsistrar salida es igual, metodo guardar hacemos uso del try para crear el objeto de tipo filewrite para que se abra y se cierre automaticamente, luego le pasamos el mensaje.

--- PIA/GymPOS/src/main/java/com/mycompany/gympos/controllers/ControlAccesoController.java
+++ PIA/GymPOS/src/main/java/com/mycompany/gympos/controllers/ControlAccesoController.java
@@ -5,11 +5,20 @@
 package com.mycompany.gympos.controllers;
 import com.mycompany.gympos.exceptions.MembresiaVencidaException;
 import com.mycompany.gympos.models.Cliente;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 /**
  *
  * @author Lic Carmen
  */
 public class ControlAccesoController {
+    
+    private final String ARCHIVO_LOG = "registro_accesos.txt";
+    
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    
     public void registrarEntrada(Cliente cliente) throws MembresiaVencidaException {
         if (cliente.getMembresia() == null) {
             throw new MembresiaVencidaException("El cliente no tiene una membresía asignada.");
@@ -19,6 +28,29 @@ public class ControlAccesoController {
             throw new MembresiaVencidaException("ACCESO DENEGADO: La membresía de " + cliente.getNombre() + " está vencida.");
         }
         
+        
+        String fechaHora = LocalDateTime.now().format(formatter);
+        String mensaje = "[ENTRADA] " + fechaHora + " - " + cliente.getNombre() + " (ID: " + cliente.getIdCliente() + ")\n";
+        
+        guardarEnLog(mensaje);
+        
         System.out.println("ACCESO CONCEDIDO: Bienvenido " + cliente.getNombre());
     }
+    
+    public void registrarSalida(Cliente cliente) {
+        String fechaHora = LocalDateTime.now().format(formatter);
+        String mensaje = "[ SALIDA] " + fechaHora + " - " + cliente.getNombre() + " (ID: " + cliente.getIdCliente() + ")\n";
+        
+        guardarEnLog(mensaje);
+        System.out.print(mensaje);
+    }
+    
+    private void guardarEnLog(String mensaje) {
+        try (FileWriter fw = new FileWriter(ARCHIVO_LOG, true)) {
+            fw.write(mensaje);
+        } catch (IOException e) {
+            System.err.println("Error al guardar el log de acceso: " + e.getMessage());
+        }
+    }
+    
 }
